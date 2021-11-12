daf857e5807f63319c52dab065255577e6d219b0
ornery-mouse <cory.sorel@gmail.com>
2021-11-12 10:39:36 -0600

    inital commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejsLayouts = require('express-ejs-layouts')
@@ -5,7 +6,6 @@ const session = require('express-session')
 const passport = require('./config/ppConfig')
 const flash = require('connect-flash')
 const isLoggedIn = require('./middleware/isLoggedIn')
-
 
 // views (ejs and layouts) set up
 app.set('view engine', 'ejs')
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:false}))
 
 // session middleware
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.SUPER_SECRET_SECRET,
     resave: false,
     saveUninitialized: true
 }))

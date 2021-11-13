75febf71922dd1e38d4bd315837db8e405486b96
ornery-mouse <cory.sorel@gmail.com>
2021-11-13 01:39:57 -0600

    streams views initialized

--- controllers/streams.js
+++ controllers/streams.js
@@ -5,7 +5,11 @@ const passport = require('../config/ppConfig.js')
 const isLoggedIn = require('../middleware/isLoggedIn')
 
 router.get('/', isLoggedIn, (req, res) => {
-    res.send('Streams Page')
+    res.render('streams/streamIndex')
+})
+
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('streams/newStream')
 })
 
 module.exports = router
